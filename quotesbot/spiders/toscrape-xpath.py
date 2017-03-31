0f27b14cb181dcddacf60efac2b340f84dfbd2e9
Leon.Nie <leonnie52@163.com>
2017-03-31 17:25:52 +0800

    JsonItemExporter added

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from quotesbot.items import QuotesbotItem
 
 
 class ToScrapeSpiderXPath(scrapy.Spider):
@@ -10,13 +11,19 @@ class ToScrapeSpiderXPath(scrapy.Spider):
 
     def parse(self, response):
         for quote in response.xpath('//div[@class="quote"]'):
-            yield {
-                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
-                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
-                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
-            }
+            item = QuotesbotItem()
+
+            item['text'] = quote.xpath('./span[@class="text"]/text()').extract_first()
+            item['author'] = quote.xpath('.//small[@class="author"]/text()').extract_first()
+            item['tags'] = quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+
+            yield item
+            # yield {
+            #     'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
+            #     'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
+            #     'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+            # }
 
         next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
-
